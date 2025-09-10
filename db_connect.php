c75d6d1a7d68ab0bbce3f50550d9d2c908a45b54
Adrian Lim <adrianlim0120@gmail.com>
2025-09-10 18:55:10 +0800

    commit

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,8 @@
 <?php
 // $conn = new mysqli("localhost", "root", "", "sinderella_db");
-$conn = new mysqli("sql212.infinityfree.com", "if0_39899896", "j5yaUd1JfBFji", "if0_39899896_sinderella");
+ $conn = new mysqli("sql212.infinityfree.com", "if0_39899896", "j5yaUd1JfBFji", "if0_39899896_sinderella");
+// $conn = new mysqli("mysql-sinderella.alwaysdata.net", "429732", "Adrian@0120", "sinderella_db");
+
 $conn->set_charset('utf8mb4');
 
 if ($conn->connect_error) {
